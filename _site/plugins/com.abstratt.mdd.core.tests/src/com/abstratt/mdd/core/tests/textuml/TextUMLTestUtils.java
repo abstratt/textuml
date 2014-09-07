20b0b9e46009bbf1743ae854e70de0c030adaca6
Rafael Chaves <rafael@abstratt.com>
2014-09-07 11:38:56 -0300

    deleted site

--- _site/plugins/com.abstratt.mdd.core.tests/src/com/abstratt/mdd/core/tests/textuml/TextUMLTestUtils.java
+++
@@ -1,7 +0,0 @@
-package com.abstratt.mdd.core.tests.textuml;
-
-public class TextUMLTestUtils {
-	public static String replaceTags(String original, String lineEnding, String indentation) {
-		return original.replaceAll("<LE>", lineEnding).replaceAll("<TAB>", indentation);
-	}
-}
--- _site/plugins/com.abstratt.resman/src/com/abstratt/resman/ResourceKey.java
+++
@@ -1,7 +0,0 @@
-package com.abstratt.resman;
-
-import java.io.Serializable;
-
-public interface ResourceKey extends Serializable {
-
-}
