5e02497cfd7a625293c60d3e7f4f4a5f94fccc02
Rafael Chaves <rafael@abstratt.com>
2015-06-06 23:13:07 -0300

    avoid println

--- plugins/com.abstratt.mdd.modelrenderer.uml2dot/src/com/abstratt/mdd/modelrenderer/uml2dot/RegionRenderer.java
+++ plugins/com.abstratt.mdd.modelrenderer.uml2dot/src/com/abstratt/mdd/modelrenderer/uml2dot/RegionRenderer.java
@@ -1,19 +1,14 @@
 package com.abstratt.mdd.modelrenderer.uml2dot;
 
 import org.eclipse.uml2.uml.Region;
-import org.eclipse.uml2.uml.StateMachine;
 
-import com.abstratt.modelrenderer.IEObjectRenderer;
 import com.abstratt.modelrenderer.IRenderingSession;
 import com.abstratt.modelrenderer.IndentedPrintWriter;
+import com.abstratt.modelrenderer.RenderingUtils;
 
 public class RegionRenderer implements IElementRenderer<Region> {
     @Override
     public boolean renderObject(Region element, IndentedPrintWriter out, IRenderingSession context) {
-        element.getSubvertices().forEach(it -> {
-            System.out.println(it.eClass().getName());
-            context.render(it);
-        });
-        return true;
+        return RenderingUtils.renderAll(context, element.getSubvertices());
     }
 }
