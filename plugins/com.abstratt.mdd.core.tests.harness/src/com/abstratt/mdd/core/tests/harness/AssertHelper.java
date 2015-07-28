edb821712c9ec519deaf3972f5d295708529eda0
Rafael Chaves <rafael@abstratt.com>
2015-07-28 20:54:26 -0300

    assertion helper

--- plugins/com.abstratt.mdd.core.tests.harness/src/com/abstratt/mdd/core/tests/harness/AssertHelper.java
+++ plugins/com.abstratt.mdd.core.tests.harness/src/com/abstratt/mdd/core/tests/harness/AssertHelper.java
@@ -31,4 +31,27 @@ public class AssertHelper {
 	public static void assertStringsEqual(String seq1, String seq2) {
         Assert.assertEquals(trim(seq1), trim(seq2));
     }
+	
+	public static void assertStringStartsAndEndsWith(String seq1, String seq2, String toTest) {
+		assertStringStartsWith(seq1, toTest);
+		assertStringEndsWith(seq2, toTest);
+	}
+	
+	public static void assertStringStartsWith(String seq1, String seq2) {
+        String expectedPrefix = trim(seq1);
+		String actualString = trim(seq2);
+		Assert.assertTrue(expectedPrefix.length() <= actualString.length());
+		int minLength = expectedPrefix.length();
+		Assert.assertNotEquals("Length not expected to be zero", 0, minLength);
+		Assert.assertEquals(expectedPrefix, actualString.substring(0, minLength));
+    }
+	
+	public static void assertStringEndsWith(String seq1, String seq2) {
+        String expectedSuffix = trim(seq1);
+		String actualString = trim(seq2);
+		Assert.assertTrue(expectedSuffix.length() <= actualString.length());		
+		int minLength = expectedSuffix.length();
+		Assert.assertNotEquals("Length not expected to be zero", 0, minLength);
+		Assert.assertEquals(expectedSuffix, actualString.substring(actualString.length() - expectedSuffix.length()));
+    }
 }
