7cf1aacd0302f7f4a64765a84f754bc5cba6db91
Rafael Chaves <rafael@abstratt.com>
2018-09-14 13:37:41 -0300

    only render public element imports

--- plugins/com.abstratt.mdd.modelrenderer.uml2dot/src/com/abstratt/mdd/modelrenderer/uml2dot/ElementImportRenderer.java
+++ plugins/com.abstratt.mdd.modelrenderer.uml2dot/src/com/abstratt/mdd/modelrenderer/uml2dot/ElementImportRenderer.java
@@ -1,6 +1,7 @@
 package com.abstratt.mdd.modelrenderer.uml2dot;
 
 import org.eclipse.uml2.uml.ElementImport;
+import org.eclipse.uml2.uml.VisibilityKind;
 
 import com.abstratt.mdd.core.util.MDDExtensionUtils;
 import com.abstratt.mdd.modelrenderer.IRenderingSession;
@@ -11,6 +12,8 @@ public class ElementImportRenderer implements IElementRenderer<ElementImport>{
 	@Override
 	public boolean renderObject(ElementImport element, IndentedPrintWriter out, IRenderingSession session) {
 		if (MDDExtensionUtils.isLibrary(element.getNearestPackage()))
+			return false;
+		if (element.getVisibility() != VisibilityKind.PUBLIC_LITERAL)
 			return false;
 		return session.render(element.getImportedElement());
 	}
